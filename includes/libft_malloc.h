59a5807c20170047648f33f2eac729a1feac6200
Flavien <fkoehler@student.42.fr>
2017-09-07 11:02:21 +0200

    fix libft inclusion, added includes folder

---
+++ includes/libft_malloc.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_malloc.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkoehler <fkoehler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/05 16:35:51 by fkoehler          #+#    #+#             */
+/*   Updated: 2017/09/05 20:04:36 by fkoehler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_MALLOC_H
+# define LIBFT_MALLOC_H
+
+# include <libft.h>
+
+typedef struct		s_malloc
+{
+
+}					t_malloc;
+
+#endif
